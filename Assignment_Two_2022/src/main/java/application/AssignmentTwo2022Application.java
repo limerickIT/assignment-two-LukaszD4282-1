b4e13a673fdbfd4c29b6ee06b9758d0cb2c9cf1b
LovelyCans <61227161+LovelyCans@users.noreply.github.com>
2022-03-22 11:07:31 +0000

    Added swagger API documentation

--- Assignment_Two_2022/src/main/java/application/AssignmentTwo2022Application.java
+++ Assignment_Two_2022/src/main/java/application/AssignmentTwo2022Application.java
@@ -7,14 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan({"service", "controller", "config"})
 @EntityScan("model")
 @EnableJpaRepositories("repository")
+@EnableSwagger2
 public class AssignmentTwo2022Application {
 
 	public static void main(String[] args) {
+            
 		SpringApplication.run(AssignmentTwo2022Application.class, args);
 	}
 
---
+++ Assignment_Two_2022/src/main/java/config/SpringFoxConfig.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.awt.image.BufferedImage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SpringFoxConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+}
